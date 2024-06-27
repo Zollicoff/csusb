337d01d4a2f852288232de159c5fedcdf147de07
Zollicoff <zollicoff@gmail.com>
2024-06-27 17:51:30 +0000

    Jun 27, 2024, 10:51 AM

--- cse2020_summer24/module08/lab10/lab10.cpp
+++ cse2020_summer24/module08/lab10/lab10.cpp
@@ -1,42 +1,29 @@
-// lab10.cpp
 #include "Set.hpp"
+#include <iostream>
 using namespace std;
 
 // Print the set function
 template <typename C> 
-void print(const Set<C> & s){ 
-    for (typename  Set<C>::iterator itr = s.begin(); itr != 
-        s.end(); ++itr) 
+void print(const Set<C> & s) { 
+    for (typename Set<C>::iterator itr = s.begin(); itr != s.end(); ++itr) 
         cout << *itr << ","; 
 }
 
 // Overload the + operator to return the union of two sets
 template <typename C> 
 Set<C> operator+(const Set<C> & s1, const Set<C> & s2) {
-    Set<C> result; 
-    for (typename Set<C>::iterator itr = s1.begin(); itr != 
-    s1.end(); ++itr) 
-        result.insert(*itr);
-
-    for (typename Set<C>::iterator itr = s2.begin(); itr != 
-    s2.end(); ++itr) 
+    Set<C> result = s1; 
+    for (typename Set<C>::iterator itr = s2.begin(); itr != s2.end(); ++itr) 
         result.insert(*itr); 
-    
     return result; 
 }
 
 // Overload the - operator to return the difference of two sets
 template <typename C> 
 Set<C> operator-(const Set<C> & s1, const Set<C> & s2) { 
-    Set<C> result; 
-    for (typename Set<C>::iterator itr = s1.begin(); itr != 
-    s1.end(); ++itr) 
-        result.insert(*itr);
-    
-    for (typename Set<C>::iterator itr = s2.begin(); itr != 
-    s2.end(); ++itr) 
+    Set<C> result = s1; 
+    for (typename Set<C>::iterator itr = s2.begin(); itr != s2.end(); ++itr) 
         result.remove(*itr); 
-    
     return result; 
 }
 
@@ -53,24 +40,24 @@ Set<C> operator*(const Set<C> & s1, const Set<C> & s2) {
 int main() {
     int x = 0;
     Set<int> setA;
-	
-    cout << "insert the values to setA (stop when entering 0):" << endl;
+    
+    cout << "Insert the values to setA (stop when entering 0):" << endl;
     while (cin >> x && x != 0) {
         setA.insert(x);
     }
     
-    cout << "print the values:" << endl;
+    cout << "Print the values:" << endl;
     print(setA);
     cout << endl;
   
     Set<int> setB;
 
-    cout << "insert the values to SetB (stop when entering 0):" << endl;
+    cout << "Insert the values to setB (stop when entering 0):" << endl;
     while (cin >> x && x != 0) {
         setB.insert(x);
     }
     
-    cout << "print the values:" << endl;
+    cout << "Print the values:" << endl;
     print(setB);
     cout << endl;
  
