7a5c73f909170c5b07d694f80cee8147dbd44dff
Zachary A. Hampton <95104559+Zollicoff@users.noreply.github.com>
2024-06-28 05:26:11 +0000

    Jun 28, 2024, 5:26 AM

---
+++ cse2020_summer24/module08/lab11/pair.hpp
@@ -0,0 +1,66 @@
+// Pair.cpp
+// for class Map, after Mark A. Weiss, Kerstin Voigt
+
+#ifndef PAIR_H_
+#define PAIR_H_
+using namespace std;
+
+template <typename K, typename V>
+class Pair
+{
+ public:
+
+  Pair() {}
+
+  Pair(K thekey):key(thekey)
+  {}
+  
+  Pair(K thekey, V theval):key(thekey), value(theval) 
+  {}
+
+  K getKey() const
+  {
+    return key;  
+  }
+  
+  V& getValue()
+  {
+    return value;  
+  }  
+
+  void setKey(K k)
+  {
+     key = k;
+  }
+
+  void setValue(V v)
+  {
+     value = v;
+  }
+  
+  bool operator == (const Pair<K,V>& rhs) const
+  {
+    return  key == rhs.key;
+  }
+
+  bool operator != (const Pair<K,V>& rhs) const
+  {
+    return key != rhs.key;
+  }
+  
+  bool operator < (const Pair<K,V>& rhs) const
+  {
+    return key < rhs.key;
+  }
+  
+  bool operator > (const Pair<K,V>& rhs) const
+  {
+    return key > rhs.key;
+  }
+
+ private:
+  K key;
+  V value;
+};
+
+#endif
