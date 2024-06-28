c1c8bf668ae826a635e04a9529249143ed2f47dd
Zollicoff <zollicoff@gmail.com>
2024-06-28 01:02:03 +0000

    Jun 27, 2024, 6:02 PM

--- cse2020_summer24/module08/lab10/lab10.cpp
+++ cse2020_summer24/module08/lab10/lab10.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 // Add 4 functions
 template <typename C>
-void print(const Set<C> & s) {
-    for (typename Set<C>::iterator itr = s.begin(); itr != s.end(); ++itr) {
+void print(const Set<C> & s)
+{
+    for (typename Set<C> :: iterator itr = s.begin(); itr != s.end(); ++itr) 
         cout << *itr << ",";
-    }
 }
 
 // Union of two sets
@@ -16,13 +16,11 @@ template <typename C>
 Set<C> operator+(const Set<C> & s1, const Set<C> & s2) {
     Set<C> result;
     
-    for (typename Set<C>::iterator itr = s1.begin(); itr != s1.end(); ++itr) {
+    for (typename Set<C>::iterator itr = s1.begin(); itr != s1.end(); ++itr)
         result.insert(*itr);
-    }
     
-    for (typename Set<C>::iterator itr = s2.begin(); itr != s2.end(); ++itr) {
+    for (typename Set<C>::iterator itr = s2.begin(); itr != s2.end(); ++itr)
         result.insert(*itr);
-    }
     
     return result;
 }
@@ -62,9 +60,9 @@ int main() {
     Set<int> setA;
     cout << "insert the values to setA (stop when entering 0):" << endl;
     cin >> x;
-    while (true) {
-        if (x == 0) break;
+    while (x != 0) {
         setA.insert(x);
+        cin >> x;
     }
     
     cout << "print the values:" << endl;
@@ -74,9 +72,9 @@ int main() {
     Set<int> setB;
     cout << "insert the values to SetB (stop when entering 0):" << endl;
     cin >> x;
-    while (true) {
-        if (x == 0) break;
+    while (x != 0) {
         setB.insert(x);
+        cin >> x;
     }
 
     cout << "print the values:" << endl;
