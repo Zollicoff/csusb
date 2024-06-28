4d451fab5289f5fc118751bf8475cc198d47f2d7
Zollicoff <zollicoff@gmail.com>
2024-06-28 12:35:12 -0700

    -

--- cse2020_summer24/module09/lab12/lab12.cpp
+++ cse2020_summer24/module09/lab12/lab12.cpp
@@ -1,16 +1,45 @@
-// lab12.cpp
+#include <iostream>
 #include <string>
 #include "BinaryHeap.cpp"
 
-// Test program
-int main( )
+using namespace std;
+
+int main()
 {
     BinaryHeap<int> printQueue;
     int x;
-    cout << "Enter the priority of print job: ";
-    cin >> x;
 
-    // add your code
-     
+    // Input priority values and insert them into the queue
+    while (true) {
+        cout << "Enter the priority of print job (or -1 to finish): ";
+        cin >> x;
+        
+        if (x == -1) break;
+        
+        printQueue.insert(x);
+    }
+
+    // Print the initial heap
+    cout << "\nThe priority values of print jobs: ";
+    printQueue.printHeap();
+    cout << endl;
+
+    // Get the number of jobs to run
+    int n;
+    cout << "\nThe number of jobs the printer will run? ";
+    cin >> n;
+
+    // Delete the minimum n times
+    for (int i = 0; i < n; i++) {
+        if (!printQueue.isEmpty()) {
+            printQueue.deleteMin();
+        }
+    }
+
+    // Print the remaining jobs
+    cout << "\nThe priority values of remaining print jobs: ";
+    printQueue.printHeap();
+    cout << endl;
+
     return 0;
 }
