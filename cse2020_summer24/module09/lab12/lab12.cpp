bc33afac973d6f92b5e579b87db4b8a99191fb77
Zollicoff <zollicoff@gmail.com>
2024-06-28 19:43:50 +0000

    Jun 28, 2024, 12:43 PM

--- cse2020_summer24/module09/lab12/lab12.cpp
+++ cse2020_summer24/module09/lab12/lab12.cpp
@@ -1,3 +1,18 @@
+/*
+ * Name: Zachary A. Hampton
+ * Student ID: 008339494
+ * Assignment: Lab 12
+ * Date: June 28, 2024
+ * 
+ * This program uses a binary heap to simulate a print queue.
+ * The user inputs the priority of print jobs, and the program inserts 
+ * them into the queue. The user then inputs the number of jobs the
+ * printer will run, and the program deletes the minimum priority job
+ * that many times. Finally, the program prints the remaining jobs in
+ * the queue.
+*/
+
+// lab12.cpp
 #include <iostream>
 #include <string>
 #include "BinaryHeap.hpp"
