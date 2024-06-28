88caa57f0b5185b91a26f18b003353fdb9a2f82a
Zollicoff <zollicoff@gmail.com>
2024-06-28 12:30:27 -0700

    -

---
+++ cse2020_summer24/module09/lab12/lab12.cpp
@@ -0,0 +1,16 @@
+// lab12.cpp
+#include <string>
+#include "BinaryHeap.cpp"
+
+// Test program
+int main( )
+{
+    BinaryHeap<int> printQueue;
+    int x;
+    cout << "Enter the priority of print job: ";
+    cin >> x;
+
+    // add your code
+     
+    return 0;
+}
