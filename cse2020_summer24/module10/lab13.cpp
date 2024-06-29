5a5206e0b9a2fb9bd3c7d638a32ec6b2ecd668ad
Zollicoff <zollicoff@gmail.com>
2024-06-29 20:04:02 +0000

    Jun 29, 2024, 1:04 PM

---
+++ cse2020_summer24/module10/lab13.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include "MergeSort.hpp"
+
+using namespace std;
+
+int main()
+{
+	vector <int> v;
+    
+    // add your code
+    ifstream input("sorting.txt");
+    int number;
+    while (input >> number) {
+        v.push_back(number);
+    }
+    input.close();
+    
+    return 0;
+}
