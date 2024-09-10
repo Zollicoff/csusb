de824ab52a8133fe4eb528f2a4e3ff47f7b47a35
Zollicoff <zollicoff@gmail.com>
2024-09-10 10:42:56 -0700

    -

--- cse1300L_fall24/assignment_2-3.py
+++ cse1300L_fall24/assignment_2-3.py
@@ -1,28 +1,3 @@
-# Given the user inputs, complete a program that does the following tasks:
-
-    # Define a set, fruits, containing the user inputs myFruit1, myFruit2, and myFruit3.
-    # Add the user input theirFruit to fruits.
-    # Find the intersection of fruits and yourFruit1 and yourFruit2
-    # Remove myFruit1 from fruits.
-
-# Note: For testing purposes, sets are printed using sorted() for comparison.
-
-# Ex: If the input is:
-
-# apple
-# peach
-# lemon
-# apple
-# pear
-# plum
-
-# the output is:
-
-# ['apple', 'lemon', 'peach']
-# ['apple', 'lemon', 'peach', 'plum']
-# ['apple']
-# ['lemon', 'peach', 'plum']
-
 myFruit1 = input()
 myFruit2 = input()
 myFruit3 = input()
