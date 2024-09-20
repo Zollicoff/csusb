bac2be8c2c13aec07dcf931570d90be756280368
Zollicoff <zollicoff@gmail.com>
2024-09-20 18:38:48 +0000

    Sep 20, 2024, 11:38 AM

---
+++ cse1300_fall24/section2/homework2_3/cse1300_homework_2_3_1_008339494.py
@@ -0,0 +1,14 @@
+# Print the return value of the function subtractValues() for x=6 and y=1.
+
+# The code provided defines the function
+
+# Code:
+
+# Defines the function subtractValues
+def subtractValues(x, y):
+    result = x - y
+    return result
+
+# Prints the output of the function subtractValues() for x=6 and y=1
+print(subtractValues(6, 1))
+
