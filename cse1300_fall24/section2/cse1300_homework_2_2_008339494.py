894801b4ed1a9c32b27bfc8dc10c25840cd1c09d
Zollicoff <zollicoff@gmail.com>
2024-09-20 09:12:16 -0700

    -

---
+++ cse1300_fall24/section2/cse1300_homework_2_2_008339494.py
@@ -0,0 +1,31 @@
+    # Create a list practiceList1 with elements 3, 'b', and 11.
+    # Replace the third element with 'v'.
+    # Append 26 to practiceList1.
+    # Remove the first element from practiceList1.
+    # Combine practiceList1 with the list practiceList2.
+
+# The code provided displays all results and creates the second list, practiceList2.
+
+# Code:
+
+practiceList2 = ['cat', 105]
+
+# Create a list with elements 3, 'b', 11
+practiceList1 = [3, 'b', 11]
+print(practiceList1)
+
+# Replace the third element with 'v'
+practiceList1[2] = 'v'
+print(practiceList1)
+
+# Append 26 to the list
+practiceList.append(26)
+print(practiceList1)
+
+# Remove the first element from the list
+my_list.pop[0]
+print(practiceList1)
+
+# Combine practiceList1 and practiceList2
+combinedList = practiceList1 + practiceList2
+print(combinedList)
