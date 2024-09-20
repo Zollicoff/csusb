4cd63e840f620654ab55cdb455c95af085985f89
Zollicoff <zollicoff@gmail.com>
2024-09-20 19:13:34 +0000

    Sep 20, 2024, 12:13 PM

--- cse1300_fall24/section2/homework2_5/cse1300_homework_2_5_2_008339494.py
+++ cse1300_fall24/section2/homework2_5/cse1300_homework_2_5_2_008339494.py
@@ -3,14 +3,14 @@
 # Code:
 
 # Load necessary package
-# Load necessary package
 import numpy as np
 
 # Create array
-myArr = np.array([['n', 'x', 's', 'y'], ['q', 'v', 'l', 'p']])
+myArr = [['n', 'x', 's', 'y'], ['q', 'v', 'l', 'p']]
 
 # Insert character into the last row of array
-myArr[-1] = np.append(myArr[-1], 'r')
+myArr = np.array(myArr)
+myArr[-1, 0] = 'r'
 
 # Print the array
 print(myArr)
