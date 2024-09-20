c89a6e7370dcee5757d6f5acf37cbd6ae0e172cb
Zollicoff <zollicoff@gmail.com>
2024-09-20 16:29:43 +0000

    Sep 20, 2024, 9:29 AM

---
+++ cse1300_fall24/section2/cse1300_homework_2_2_3_008339494.py
@@ -0,0 +1,19 @@
+# Create a dictionary exampleDict with indexes 'tarantula', 'hornet', and 'tuna', and values 0.11, 0.05, and 1, respectively.
+# Modify the value of the element with key 'tuna' to 0.98.
+# Remove the element with key 'hornet' from exampleDict.
+
+# The code provided prints all results.
+ 
+# Code:
+
+# Create a dictionary with indexes 'tarantula', 'hornet', and 'tuna', and values 0.11, 0.05, and 1
+exampleDict = {'tarantula': 0.11, 'hornet': 0.05, 'tuna': 1}
+print(exampleDict)
+
+# Modify the value of the element with key 'tuna' to 0.98
+
+print(exampleDict)
+
+# Remove element with key 'hornet'
+# Your code goes here
+print(exampleDict)
