c05010bc66c3528f908891ba6e9ef71733c8ba77
Zollicoff <zollicoff@gmail.com>
2024-09-20 18:53:29 +0000

    Sep 20, 2024, 11:53 AM

---
+++ cse1300_fall24/section2/homework2_5/cse1300_homework_2_5_1_008339494.py
@@ -0,0 +1,12 @@
+# Create a 2-dimensional array containing the arrays [2, 17], [18, 15], [3, 6], [1, 9], [5, 4], [12, 19], [10, 13].
+
+# Code:
+
+# Load the necessary package
+import numpy as np
+
+# Create an array
+myArray = [[2, 17], [18, 15], [3, 6], [1, 9], [5, 4], [12, 19], [10, 13]]
+
+# Print the array
+print(myArray)
