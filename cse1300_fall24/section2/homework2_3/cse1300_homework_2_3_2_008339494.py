3f1316e9e865cd97cd12417997216e562639b189
Zollicoff <zollicoff@gmail.com>
2024-09-20 18:42:53 +0000

    Sep 20, 2024, 11:42 AM

---
+++ cse1300_fall24/section2/homework2_3/cse1300_homework_2_3_2_008339494.py
@@ -0,0 +1,11 @@
+# Fill in the function call so that the output is: 6/1/2021 11:46
+
+# The code provided defines the function.
+
+# Code:
+
+def showTime(year, month, day, hour, minutes):
+    print(str(month) + '/' + str(day) + '/' + str(year) + ' ' + str(hour) + ':' + str(minutes))
+
+showTime(2021, 6, 1, 11, 46)
+
