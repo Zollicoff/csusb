d0d5d52c687b6f3fbffb9d9fa90ff771f3490513
Zollicoff <zollicoff@gmail.com>
2024-09-20 19:33:53 +0000

    Sep 20, 2024, 12:33 PM

---
+++ cse1300_fall24/section2/homework2_5/cse1300_homework_2_5_5_008339494.py
@@ -0,0 +1,15 @@
+# Return the logarithm of elements in myArr.
+
+# Code
+
+# Load necessary package
+import numpy as np
+
+# Create array
+myArr = [[40, 42, 24], [13, 26, 43]]
+
+# Find logarithm of array elements
+myArr = np.log(myArr)
+
+# Print the array
+print(myArr)
