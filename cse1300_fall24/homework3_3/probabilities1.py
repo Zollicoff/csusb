6b56ceb1cb108a0a2b3288f6cf9f72888baba91f
Zollicoff <zollicoff@gmail.com>
2024-10-04 20:41:46 +0000

    Oct 4, 2024, 1:41 PM

---
+++ cse1300_fall24/homework3_3/probabilities1.py
@@ -0,0 +1,17 @@
+# Given probabilities
+P_A = 0.3            # P(A): Probability of chronic kidney disease (CKD)
+P_B = 0.825          # P(B): Probability of receiving IV chemotherapy
+P_A_and_B = 0.195    # P(A and B): Probability of both A and B occurring
+
+# Compute additional probabilities
+P_A_or_B = P_A + P_B - P_A_and_B  # P(A or B)
+P_A_given_B = P_A_and_B / P_B     # P(A|B): Probability of A given B
+P_B_given_A = P_A_and_B / P_A     # P(B|A): Probability of B given A
+
+# Output the probabilities
+print("P(A) =", P_A)
+print("P(B) =", P_B)
+print("P(A and B) =", P_A_and_B)
+print("P(A or B) =", P_A_or_B)
+print("P(A|B) =", P_A_given_B)
+print("P(B|A) =", P_B_given_A)
