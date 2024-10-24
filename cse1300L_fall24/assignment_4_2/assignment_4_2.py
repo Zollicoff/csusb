095271993ee9fd365631c1d4eee7e76510d0b289
Zollicoff <zollicoff@gmail.com>
2024-10-24 03:43:09 +0000

    Oct 23, 2024, 8:43 PM

--- cse1300L_fall24/assignment_4_2/assignment_4_2.py
+++ cse1300L_fall24/assignment_4_2/assignment_4_2.py
@@ -12,13 +12,14 @@ import sqlite3 # sql package
 import warnings
 
 warnings.filterwarnings("ignore")
-# read csvfile movie1.csv
 
+# read csvfile movie1.csv
 movie = pd.read_csv('movie1.csv')
 pd.set_option('display.notebook_repr_html', False)
 def create_connection():
     return sqlite3.connect(':memory:')
 conn = create_connection()
+
 # take data stored in Pandas DataFrame (df) and writes it into a SQL database table
 movie.to_sql('movie', conn, if_exists='replace', index=False)
 movie_query = """
