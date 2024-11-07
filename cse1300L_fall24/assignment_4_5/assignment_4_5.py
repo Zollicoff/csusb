77021c13a3015b99ce59d8c0e0a3dc7a5d1518b1
Zollicoff <zollicoff@gmail.com>
2024-11-07 05:01:34 +0000

    Nov 6, 2024, 9:01 PM

---
+++ cse1300L_fall24/assignment_4_5/assignment_4_5.py
@@ -0,0 +1,38 @@
+#The Horse table has the following columns:
+# ID - integer, primary key
+# RegisteredName - variable-length string
+# Breed - variable-length string
+# Height - decimal number
+# BirthDate - date
+# Write a SELECT statement to select the registered name and height for only horses that have an above average height. Order the results by height (ascending).
+# Hint: Use a subquery to find the average height.
+
+# Import the required libraries
+import pandas as pd
+import sqlite3
+import warnings
+
+# Suppress warnings
+warnings.filterwarnings("ignore")
+pd.set_option('display.notebook_repr_html', False)
+
+# Create a connection to an in-memory SQLite database
+def create_connection():
+    return sqlite3.connect(':memory:')
+conn = create_connection()
+
+# Load the CSV file into a Pandas DataFrame
+horse_df = pd.read_csv('horse1-1.csv')
+
+# Write the DataFrame to the SQL database
+horse_df.to_sql('Horse', conn, index=False, if_exists='replace')
+
+# SQL query to perform the LEFT JOIN and retrieve the desired columns
+horse_query = """
+SELECT Horse.Name, Horse.Breed, Horse.Age
+FROM Horse
+"""
+
+# Execute the query and load the result into a DataFrame
+horse = pd.read_sql_query(horse_query, conn)
+print(horse)
