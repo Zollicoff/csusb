184424377b898084f9e4a9e718bb98ed071a4d6d
Zollicoff <zollicoff@gmail.com>
2025-02-05 20:35:37 -0800

    -

--- cse4010_spring25/lab1b/wireTest2.v
+++ cse4010_spring25/lab1b/wireTest2.v
@@ -1,16 +1,24 @@
-// Accepts two inputs W and X
-// Gives two outputs Y and Z
-// Set Y equal to the negation of X
-// Set Z equal to the negation of Y
+/*
+Instructions from Dr. Ahmed:
+Accepts two inputs W and X
+Gives two outputs Y and Z
+Set Y equal to the negation of X
+Set Z equal to the negation of Y
+*/
 
-module wireTest2 (W,X,Y,Z)
+// Define the wireTest2 module
+module wireTest2 (W,X,Y,Z);
 
+    // Declare the inputs and outputs
     input W;
     input X;
     output Y;
     output Z;
 
+    // Assign Y to the negation of X
     assign Y = !X;
+    // Assign Z to the negation of Y
     assign Z = !Y;
 
+// End the wireTest2 module
 endmodule
--- cse4010_spring25/lab1b/wireTest2_tb.v
+++ cse4010_spring25/lab1b/wireTest2_tb.v
@@ -1,44 +1,60 @@
-// Initially sets W and X equal to 0;
-// b. Wait 20 nanoseconds, then set W to 1.
-// c. Wait 20 more nanoseconds, then set X to 1.
-// d. Wait 20 more nanoseconds, then set W to 0.
-// e. Wait 20 more nanoseconds, then set X to 0.
+/*
+Instructions from Dr. Ahmed:
+This file is used to test the wireTest2 module.
+The testbench will set W and X to 0 and 1 in various combinations
+and check that Y and Z are set correctly.
+The testbench will output "Wire test complete!" when the test is done.
+*/
 
+// Include the wireTest2 module and timescale of 1ns
 `timescale 1ns / 1ns
 `include "wireTest2.v"
 
+// Define the wireTest2_tb module
 module wireTest2_tb;
 
+// Declare the inputs and outputs
 reg W;
 reg X;
 wire Y;
 wire Z;
 
+// Instantiate the wireTest2 module
 wireTest2 uut (W, X, Y, Z);
 
+// Define the initial block
 initial begin
 
+    // Dump the waveforms to wireTest2_tb.vcd
     $dumpfile("wireTest2_tb.vcd");
-    $dumpvars(0, wiretest2_tb);
+    $dumpvars(0, wireTest2_tb);
 
+    // Set W and X to 0
     W = 0;
     X = 0;
     #20
 
+    // Set W to 1
     W = 1;
     #20
 
+    // Set X to 1
     X = 1;
     #20
 
+    // Set W to 0
     W = 0;
     #20
 
+    // Set X to 0
     X = 0;
     #20
 
-    $display("Wire test complete!")
+    // Print "Wire test complete!" to the console
+    $display("Wire test complete!");
 
+// End the initial block
 end
 
+// End the wireTest2_tb module
 endmodule
